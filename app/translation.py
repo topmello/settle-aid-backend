d44951634e419a08c34b62d273e740c65b3dd118
TonyJiashiChen <tonychen9661@gmail.com>
2023-08-29 14:20:51 +1000

    add get lang code

--- app/translation.py
+++ app/translation.py
@@ -1,8 +1,18 @@
+import json
 import os
 from google.cloud import translate
 
+def get_code(source):
+    f = open('../data/lang_to_code.json')
+    codes = json.load(f)
 
-def translate_text(text, project_id="fleet-fortress-395004"):
+    for i in codes:
+        if i == source:
+            print(codes[i])
+            return codes[i]
+
+
+def translate_text(text, source, project_id="fleet-fortress-395004"):
     credential_path = "./google_application_credentials.json"
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
@@ -10,13 +20,16 @@ def translate_text(text, project_id="fleet-fortress-395004"):
     location = "global"
     parent = f"projects/{project_id}/locations/{location}"
 
+    lang_code = get_code(source)
+    print(lang_code)
+
     response = client.translate_text(
         request={
             "parent": parent,
             "contents": [text],
             "mime_type": "text/plain",
-            "source_language_code": "en-US",
-            "target_language_code": "zh",
+            "source_language_code": lang_code,
+            "target_language_code": "en",
         }
     )
 
@@ -24,4 +37,5 @@ def translate_text(text, project_id="fleet-fortress-395004"):
         print("Translated text: {}".format(translation.translated_text))
 
 
-translate_text("hi")
+
+translate_text("你好", "Chinese (Simplified)")
