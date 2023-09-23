ab84376655ef744edae02da49635e10d93021935
jirathip-k <jkun0008@student.monash.edu>
2023-09-23 10:59:16 +1000

    minor fix

--- app/routers/challenge.py
+++ app/routers/challenge.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 
 router = APIRouter(
-    prefix='/challange',
-    tags=["Challange"]
+    prefix='/challenge',
+    tags=["Challenge"]
 )
 
 
-@route.get("/")
-async def get_challange():
+@router.get("/")
+async def get_challenge():
     return {"message": "Hello World"}
